b88049fbeb5c7bb7c437686832dcb67b50523647
Alex Schmoe <aschmoe@gmail.com>
2014-05-12 14:42:27 -0700

    updated flight input formats version to latest

--- profiles/flight/modules/features/flight_input_formats/js/flightInputFormat.js
+++ profiles/flight/modules/features/flight_input_formats/js/flightInputFormat.js
@@ -1,17 +1,20 @@
-/* New D7 Wrapper --> */(function ($) {
+(function ($) {
 
 
 Drupal.behaviors.flightInputFormat = {
   attach: function(context, settings) { //new attach function
     // alter settings
-    // @todo: re-enable this? It was throwing a fatal js err - JL Feb 22, 2014
-    if(settings.ckeditor && settings.ckeditor.elements && _.size(settings.ckeditor.elements)) {
+    if(settings.ckeditor 
+      && settings.ckeditor.elements 
+      && Object.keys(settings.ckeditor.elements).length) {
+
       // grab window height
       var height = "innerHeight" in window 
-               ? window.innerHeight
-               : document.documentElement.offsetHeight;
+        ? window.innerHeight
+        : document.documentElement.offsetHeight;
+        
       // run through formats 
-      _.each(settings.ckeditor.input_formats, function(format) {
+      $.each(settings.ckeditor.input_formats, function(format) {
         // restrict autogrow
         if(format.autoGrow_maxHeight && format.autoGrow_maxHeight > 0) {
           format.autoGrow_maxHeight = height * .75;
@@ -24,5 +27,5 @@ Drupal.behaviors.flightInputFormat = {
   }//attach
 };//behaviors
 
-/* New D7 Wrapper --> */ })(jQuery);
+})(jQuery);
 
