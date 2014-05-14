bcaf626c6cbe3219337d87efed24106061e8ae8d
Alex Schmoe <aschmoe@gmail.com>
2014-05-14 16:21:52 -0700

    switched away from php shorthand in template files

--- profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay-modal.tpl.php
+++ profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay-modal.tpl.php
@@ -1,9 +1,9 @@
-<div class="click-to-play click-to-play-modal<?=$additional_classes?>" id="click-to-play-wrapper-<?php print $id; ?>">
+<div class="click-to-play click-to-play-modal<?php print $additional_classes; ?>" id="click-to-play-wrapper-<?php print $id; ?>">
   <a href="#" data-reveal-id="click-to-play-<?php print $id; ?>">
     <?php print $placeholder; ?>
     <div class="<?php print $play_class; ?>" class=""></div>
   </a>
-  <div id="click-to-play-<?php print $id; ?>" class="reveal-modal flex-video<?=$additional_classes?>" data-reveal>
-    <?php print $content; ?>
+  <div id="click-to-play-<?php print $id; ?>" class="reveal-modal flex-video<?php print $additional_classes; ?>" data-reveal> 
+    <?php dpm($content); print $content; ?>
   </div>
 </div>
--- profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay.tpl.php
+++ profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay.tpl.php
@@ -1,9 +1,9 @@
-<div class="click-to-play<?=$additional_classes?>" id="click-to-play-wrapper-<?php print $id; ?>">
+<div class="click-to-play<?php print $additional_classes; ?>" id="click-to-play-wrapper-<?php print $id; ?>">
   <a href="#" data-reveal-id="click-to-play-<?php print $id; ?>">
     <?php print $placeholder; ?>
     <div class="<?php print $play_class; ?>" class=""></div>
   </a>
-  <div id="click-to-play-<?php print $id; ?>" class="reveal-modal<?=$additional_classes?>" data-reveal>
+  <div id="click-to-play-<?php print $id; ?>" class="reveal-modal" data-reveal>
     <?php print $content; ?>
   </div>
 </div>
