8ed73d8168027f35597f8b34af34e905daa8e709
Alex Schmoe <aschmoe@gmail.com>
2014-05-14 16:10:00 -0700

    upated foundation formatters to allow for additional classes for display

--- profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay-modal.tpl.php
+++ profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay-modal.tpl.php
@@ -1,9 +1,9 @@
-<div class="click-to-play click-to-play-modal" id="click-to-play-wrapper-<?php print $id; ?>">
+<div class="click-to-play click-to-play-modal<?=$additional_classes?>" id="click-to-play-wrapper-<?php print $id; ?>">
   <a href="#" data-reveal-id="click-to-play-<?php print $id; ?>">
     <?php print $placeholder; ?>
     <div class="<?php print $play_class; ?>" class=""></div>
   </a>
-  <div id="click-to-play-<?php print $id; ?>" class="reveal-modal flex-video" data-reveal>
+  <div id="click-to-play-<?php print $id; ?>" class="reveal-modal flex-video<?=$additional_classes?>" data-reveal>
     <?php print $content; ?>
   </div>
 </div>
