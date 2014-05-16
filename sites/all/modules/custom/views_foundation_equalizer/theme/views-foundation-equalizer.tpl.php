df19bdbc5e1603cf4aca299833b1621c164aea92
Alex Schmoe <aschmoe@gmail.com>
2014-05-15 18:32:08 -0700

    fixing php shorthand in tpl file

--- sites/all/modules/custom/views_foundation_equalizer/theme/views-foundation-equalizer.tpl.php
+++ sites/all/modules/custom/views_foundation_equalizer/theme/views-foundation-equalizer.tpl.php
@@ -13,14 +13,14 @@
 <?php endif; ?>
 <?php $i = 0; $len = count($rows); ?>
 <?php foreach ($rows as $id => $row): ?>
-  <? if($i % $items_per_row == 0): ?>
+  <?php if($i % $items_per_row == 0): ?>
     <div data-equalizer<?php print $row_wrapper_class; ?>>
-  <? endif; ?>
+  <?php endif; ?>
   <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"'; } print $row_attribute; ?>>
     <?php print $row; ?>
   </div>
-  <? if(($i % $items_per_row == ($items_per_row - 1)) || $i >= ($len - 1)): ?>
+  <?php if(($i % $items_per_row == ($items_per_row - 1)) || $i >= ($len - 1)): ?>
     </div>
-  <? endif; ?>
-  <? $i++; ?>
+  <?php endif; ?>
+  <?php $i++; ?>
 <?php endforeach; ?>
