777cc3d0688f2530ed7d2ee4539f2a2500cad2a2
Jeff Lyon <jeff@albatrossdigital.com>
2014-06-11 13:51:51 -0700

    Adding template file to make faq list an accordion

---
+++ profiles/flight/themes/contrib/zurb-foundation/templates/views/views-view-unformatted--faq-list--accordion.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to display a list of rows.
+ *
+ * @ingroup views_templates
+ */
+?>
+<?php if (!empty($title)): ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+
+<dl class="accordion" data-accordion="myAccordionGroup">
+
+<?php foreach ($rows as $id => $row): ?>
+  <?php $active = $id == 0 ? ' active' : ''; ?>
+  <dd<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
+    <?php print str_replace('class="content nid-', 'class="content'. $active .'" id="nid-', $row); ?>
+  </dd>
+<?php endforeach; ?>
+
+</dl>
