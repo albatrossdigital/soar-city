7a839ef75940604bdf680a95fa94b34a3544ac29
Jeff Lyon <jeff@albatrossdigital.com>
2014-06-16 01:59:36 -0700

    Updating twtitrter.php

--- sites/all/libraries/jquery-social-stream/twitter.php
+++ sites/all/libraries/jquery-social-stream/twitter.php
@@ -4,7 +4,6 @@ $consumer_key = "cUfx93n2NBU3J8VCZ1pQ";
 $consumer_secret = "FvKP8uzrVhGAJ2ncuofsSTUsLV50H2KdW9CkNrhAENo";
 $oauth_access_token = "261263099-mF9o2SrPe5vno5IcwzGE36a0L4MiGf86Ou6a0CJy"; 
 $oauth_access_token_secret = "eqgxmDB7hsBoph18758dRmkzzb0E8emyZfIm8bD9pWg";
-print 'aasdf';
 // DO NOT EDIT BELOW THIS LINE
 switch($_GET['url'])
 {
