9d3b064af948c2d4ad9c253f8ee1d91abe0083d4
Alex Schmoe <aschmoe@gmail.com>
2014-06-19 14:56:48 -0700

    adding page wrap and content wrap to external service page

---
+++ sites/all/themes/custom/flight_city/templates/page--external-service--page-wrap.tpl.php
@@ -0,0 +1,6 @@
+<div data-offcanvas role="document" class="page off-canvas-wrap"><div class="inner-wrap">
+<!-- HEADER + CONTENT GOES HERE -->
+</div></div>
+<!--/.page -->
+
+<!-- FOOTER GOES HERE -->
--- sites/all/themes/custom/flight_city/templates/page--external-service.tpl.php
+++ sites/all/themes/custom/flight_city/templates/page--external-service.tpl.php
@@ -1,6 +1,10 @@
 <div data-offcanvas role="document" class="page off-canvas-wrap"><div class="inner-wrap">
+<h1>Page wrapper</h1>
+<textarea rows="30"><?php print $external_page; ?></textarea>
 <h1>Header</h1>
 <textarea rows="30"><?php print $external_header; ?></textarea>
+<h1>Content wrapper</h1>
+<textarea rows="30"><?php print $external_content; ?></textarea>
 <h1>Footer</h1>
 <textarea rows="30"><?php print $external_footer; ?></textarea>
 <?php if ($messages && !$zurb_foundation_messages_modal): ?>
