38cd3f0ae80a5ffa19ef0458cf3293abe7e3d705
Jeff Lyon <jeff@albatrossdigital.com>
2014-06-23 12:55:06 -0700

    Cleaning up social wall

--- sites/all/modules/custom/social_stream/js/social-stream.js
+++ sites/all/modules/custom/social_stream/js/social-stream.js
@@ -37,25 +37,30 @@ Drupal.behaviors.social_stream = {
           if (feeds[value.network] == undefined) {
             feeds[value.network] = [];
           }
-          var arr = value.url.split('/');
-          switch (value.network) {
-            case 'flickr':
-              feeds[value.network].push(arr[arr.length-1].replace(/\?q\=/, ''));
-              break;
-            case 'blogger':
-              feeds['rss'].push(value.url + 'feeds/posts/default?alt=rss');
-              break;
-            case 'wordpress':
-              feeds['rss'].push(value.url + 'feed');
-              break;
-            case 'wall':
-            case 'govdelivery':
-            case 'soundcloud':
-              feeds[value.network] = undefined;
-              break;
-            default:
-              feeds[value.network].push(arr[arr.length-1]);
-          } 
+          if (settings.social_stream.max_feeds == undefined || feeds[value.network].length < settings.social_stream.max_feeds) {
+            var arr = value.url.split('/');
+            switch (value.network) {
+              case 'flickr':
+                feeds[value.network].push(arr[arr.length-1].replace(/\?q\=/, ''));
+                break;
+              case 'blogger':
+                feeds['rss'].push(value.url + 'feeds/posts/default?alt=rss');
+                break;
+              case 'wordpress':
+                feeds['rss'].push(value.url + 'feed');
+                break;
+              case 'wall':
+              case 'govdelivery':
+              case 'soundcloud':
+                feeds[value.network] = undefined;
+                break;
+              default:
+                //console.log(value.url);
+                //console.log(arr[arr.length-1]);
+                feeds[value.network].push(arr[arr.length-1]);
+            }
+          }
+          
         }
       });
       $.each(feeds, function(index, value) {
@@ -75,6 +80,13 @@ Drupal.behaviors.social_stream = {
         feeds.instagram.clientId = '3b5aa6b2bd0842eb9702f191eb5c384d';
         feeds.instagram.redirectUrl = 'http://www.baltimore.ifsight.com/social_stream';
       }
+      if (feeds.youtube != undefined) {
+        feeds.youtube.feed = 'uploads';
+      }
+      if (feeds.vimeo != undefined) {
+        feeds.vimeo.feed = 'videos';
+      }      
+      
 
       // @todo: temp?
       feeds.wordpress = undefined;
@@ -87,6 +99,10 @@ Drupal.behaviors.social_stream = {
       if (!feeds.rss.length) {
         feeds.rss = undefined;
       }
+
+      //console.log(data);
+      //console.log(feeds);
+      //return;
 
       $('#social-stream').dcSocialStream({
         feeds: feeds,
@@ -106,15 +122,21 @@ Drupal.behaviors.social_stream = {
         height: settings.social_stream.height,
       });
 
+      // Click on active social network
+      if(window.location.hash) {
+        setTimeout(function() {
+          $('#wall li>a[rel='+ window.location.hash.replace('#', '') +']').trigger('click');
+        }, 2000);
+      }
+
     });
+
+
 
     shuffle = function(o){ //v1.0
       for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
       return o;
     };
-
-
-           
 
   }
 };
