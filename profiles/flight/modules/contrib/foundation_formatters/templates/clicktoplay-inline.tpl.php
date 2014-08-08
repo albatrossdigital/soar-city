b4c47a32aef73e8cc18dd7104f6f497b85491122
Alex Schmoe <aschmoe@gmail.com>
2014-08-08 12:41:56 -0700

    attempting to fix ie9 click to play

--- profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay-inline.tpl.php
+++ profiles/flight/modules/contrib/foundation_formatters/templates/clicktoplay-inline.tpl.php
@@ -1,5 +1,5 @@
 <div class="click-to-play click-to-play-inline flex-video<?php print $additional_classes; ?>" id="click-to-play-wrapper-<?php print $id; ?>">
-  <a href="#" onclick="var e=jQuery(this).attr('data-oembed');console.log(e);jQuery(this).parent().append(e);jQuery(this).remove();return false;" data-oembed="<?php print $content; ?>">
+  <a href="#" onclick="var e=jQuery(this).attr('data-oembed');jQuery(this).parent().append(e);jQuery(this).remove();return false;" data-oembed="<?php print $content; ?>">
     <div class="click-to-play-placeholder" id="#click-to-play-<?php print $id; ?>-placeholder"><?php print $placeholder; ?></div>
     <div class="<?php print $play_class; ?>" class=""></div>
   </a>
